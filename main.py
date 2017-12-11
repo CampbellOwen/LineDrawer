360213d2e1bc5ad44ab5561eaaee11cdd45aa32f
Owen Campbell <ocampbel@sfu.ca>
2017-12-11 15:07:50 -0800

    simple test case

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from argparse import ArgumentParser
+from drawer import Drawer
 
 parser = ArgumentParser( description="Recreate given image with only lines" )
-parser.add_argument( 'filepath' )
-parser.add_argument( 'output path' )
+parser.add_argument( 'input' )
+parser.add_argument( 'output' )
 
 args = parser.parse_args()
 
-drawer = Drawer.from_file( args.filepath )
+drawer = Drawer.from_file( args.input )
+drawer.draw( args.output, 12 )
 
