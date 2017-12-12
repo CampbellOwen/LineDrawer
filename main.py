199cffeb1ec1302987f17ea61412b40db59f7bee
Owen Campbell <ocampbel@sfu.ca>
2017-12-11 17:21:14 -0800

    Added iterations cmd arg

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ from drawer import Drawer
 parser = ArgumentParser( description="Recreate given image with only lines" )
 parser.add_argument( 'input' )
 parser.add_argument( 'output' )
+parser.add_argument( 'iterations' )
 
 args = parser.parse_args()
 
 drawer = Drawer.from_file( args.input )
-drawer.draw( args.output, 12 )
+drawer.draw( args.output, int( args.iterations ) )
 
